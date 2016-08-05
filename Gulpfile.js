602b5ab7c05ebb52cbbf1fb6ec4bc496c8c9d276
Benjamin Lannon <lannonbr@clarkson.edu>
2016-08-04 22:24:59 -0400

    Getting rid of Pug for this project

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,15 +8,8 @@ gulp.task('sass', () => {
 		.pipe(gulp.dest('./css'));
 });
 
-gulp.task('pug', () => {
-	return gulp.src('./pug/*.pug')
-		.pipe(pug({pretty: true}))
-		.pipe(gulp.dest('./'));
-});
-
 gulp.task('watch', () => {
 	gulp.watch(['./sass/main.scss'], ['sass']);
-	gulp.watch(['./pug/*.pug'], ['pug']);
 });
 
 gulp.task('default', ['watch']);
